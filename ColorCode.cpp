88c00e0c07186a071e0e1ca356bd889b47eea5d4
MaryAnn04 <maryannbarnabas4@gmail.com>
2024-10-04 09:36:07 +0530

    Update ColorCode.cpp

--- ColorCode.cpp
+++ ColorCode.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <sstream> // for std::ostringstream
 
-namespace ColorCoder 
+namespace TelCoColorCoder 
 {
     const char* MajorColorNames[] = {
         "White", "Red", "Black", "Yellow", "Violet"
@@ -16,18 +16,22 @@ namespace ColorCoder
     const int numberOfMinorColors =
         sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
+    // Constructor for ColorPair
     ColorPair::ColorPair(MajorColor major, MinorColor minor):
         majorColor(major), minorColor(minor)
     {}
 
+    // Getter for MajorColor
     MajorColor ColorPair::getMajor() const {
         return majorColor;
     }
 
+    // Getter for MinorColor
     MinorColor ColorPair::getMinor() const {
         return minorColor;
     }
 
+    // ToString method
     std::string ColorPair::ToString() const {
         std::string colorPairStr = MajorColorNames[majorColor];
         colorPairStr += " ";
@@ -35,6 +39,7 @@ namespace ColorCoder
         return colorPairStr;
     }
 
+    // Function to get ColorPair from pair number
     ColorPair GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor majorColor = 
@@ -44,6 +49,7 @@ namespace ColorCoder
         return ColorPair(majorColor, minorColor);
     }
 
+    // Function to get pair number from color
     int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
         return major * numberOfMinorColors + minor + 1;
     }
